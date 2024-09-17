34ef768947cb160960d75a41bdcf2eb45a063319
MargorieGV05 <margorie.gallardo@jpschools.org>
2024-09-17 16:37:06 +0000

    add index.html and css files

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -37,19 +37,22 @@ $(function () {
 createCollectable("steve", 200, 170, 6, 0.7);
     // TODO 1
     // Create platforms
-    createPlatform(150, 720, 150, 200); 
-    createPlatform(350, 670, 80, 90);
+    createPlatform(50, 720, 190, 200); 
+    createPlatform(350, 670, 80, 50);
     createPlatform(520, 550, 100, 40);
     createPlatform(680, 430, 80, 40);
     createPlatform(780, 330, 80, 40);
-    createPlatform(880, 430, 80, 40);
-    createPlatform(1120, 320, 150, 90); 
-    createPlatform(1250, 190, 150, 60); 
-    createPlatform(990, 120, 190, 40);
-    createPlatform(500, 250, 180, 90);
-    createPlatform(100,200,200,90);
-    createPlatform(730,190,190,40);
-   createPlatform(390, 200,80,40);
+    createPlatform(880, 430, 80, 40); 
+    createPlatform(990, 250, 190, 40);
+    createPlatform(500, 250, 170, 50);
+    createPlatform(100,200,200,50);
+    createPlatform(730,150,190,40);
+    createPlatform(1190, 200,10,700);
+    createPlatform(1190, 320, 100, 30); 
+    createPlatform(1300, 190, 100, 30); 
+    createPlatform(1190, 540, 100, 30); 
+    createPlatform(1300, 440, 100, 30); 
+
 
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
@@ -59,20 +62,22 @@ createCollectable("steve", 200, 170, 6, 0.7);
     
     // TODO 2
     // Create collectables
-    createCollectable("steve", 600, 400, 20, 0.5); // creates a "steve" collectible at the coordinates (500, 300), falling with a high gravity of 20, and bouncing with 40% bounce
+    createCollectable("steve", 600, 400, 10, 0.2); // creates a "steve" collectible at the coordinates (500, 300), falling with a high gravity of 20, and bouncing with 40% bounce
     createCollectable("grace", 300, 100); // creates a "grace" collectible at the coordinates (500, 300), falling with default gravity and bouncing with default bounce %
-    createCollectable("diamond", 1000, 100);// You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
-    createCollectable("kennedi", 1200, 100);//// Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
-    createCollectable("database", 500, 200, 20, 0.5);// example usage: createCollectable(type, x, y, gravity, bounce)
+    createCollectable("diamond", 1000, 100,10,0.1);// You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
+    createCollectable("kennedi", 1200, 100, 10, 0);//// Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
+    createCollectable("database", 500, 200);// example usage: createCollectable(type, x, y, gravity, bounce)
     createCollectable("max", 500, 1000);
+    createCollectable("kennedi", 1300, 300,10,0.1);
 
 
     
     // TODO 3
     // Create cannons
-    createCannon("left", 450, 1000, 20, 50); 
-    createCannon("bottom", 990, 1000, 80, 20);
-    createCannon("top", 880, 1000, 80, 20 );
+    createCannon("left", 450, 1000, 20, 20); 
+    createCannon("bottom", 990, 1000, 20, 20);
+    createCannon("top", 880, 1000, 20, 20 );
+    createCannon("right", 790, 1000, 1000, 20 );
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
